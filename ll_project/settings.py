c559803e25fee08807ed9a9cb4ade513ff1dd1b7
Doraemon101-kk <2373459390@qq.com>
2026-02-06 02:04:47 +0800

    new

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x8m&on&z2pbe6@054$&2#w158bjl%29od+@@_=hu$bcqc3hl_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("The SECRET_KEY environment variable is not set!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
